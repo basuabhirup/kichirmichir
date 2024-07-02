f283bf68db176ac2dc7b7ec9ff715407ea210e74
basuabhirup <basu.abhirup27@gmail.com>
2024-07-03 01:04:55 +0530

    Complete all sidebar items + add Clerk UserButton

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -2,6 +2,8 @@ import { cn } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
 import SidebarItem from "@/components/sidebar-item";
+import { ClerkLoaded, ClerkLoading, UserButton } from "@clerk/nextjs";
+import { Loader } from "lucide-react";
 
 interface IProps {
   className?: string;
@@ -23,6 +25,17 @@ const Sidebar: React.FC<IProps> = ({ className }) => {
       </Link>
       <div className="flex flex-col gap-y-2 flex-1">
         <SidebarItem label="Learn" iconSrc="/learn.svg" href="/learn" />
+        <SidebarItem label="Leaderboard" iconSrc="/leaderboard.svg" href="/leaderboard" />
+        <SidebarItem label="Quests" iconSrc="/quests.svg" href="/quests" />
+        <SidebarItem label="Shop" iconSrc="/shop.svg" href="/shop" />
+      </div>
+      <div className="p-4">
+        <ClerkLoading>
+            <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
+        </ClerkLoading>
+        <ClerkLoaded>
+            <UserButton afterSignOutUrl="/" />
+        </ClerkLoaded>
       </div>
     </div>
   );
