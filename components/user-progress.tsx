edd4cfd4bade546b0b2e4c10619838e61eebf771
basuabhirup <basu.abhirup27@gmail.com>
2024-07-04 21:12:26 +0530

    Implement Sticky Wrapper section with UserProgress component

---
+++ components/user-progress.tsx
@@ -0,0 +1,47 @@
+import Link from "next/link";
+import React from "react";
+import { Button } from "./ui/button";
+import Image from "next/image";
+import { InfinityIcon } from "lucide-react";
+
+interface Props {
+  activeCourse: {
+    imgSrc: string;
+    title: string;
+  };
+  hearts: number;
+  points: number;
+  hasActiveSubscription: boolean;
+}
+
+const UserProgress: React.FC<Props> = ({ activeCourse, hearts, points, hasActiveSubscription }) => {
+  return (
+    <div className="flex items-center justify-between gap-x-2 w-full">
+      <Link href="/courses">
+        <Button variant="ghost">
+          <Image
+            src={activeCourse.imgSrc}
+            alt={activeCourse.title}
+            className="rounded-md border"
+            width={32}
+            height={32}
+          />
+        </Button>
+      </Link>
+      <Link href="/shop">
+        <Button variant="ghost" className="text-orange-500">
+          <Image src="/points.svg" height={28} width={28} alt="Points" className="mr-2" />
+          {points}
+        </Button>
+      </Link>
+      <Link href="/shop">
+        <Button variant="ghost" className="text-rose-500">
+          <Image src="/heart.svg" height={22} width={22} alt="Hearts" className="mr-2" />
+          {hasActiveSubscription ? <InfinityIcon className="h-4 w-4 stroke-[3]" /> : hearts}
+        </Button>
+      </Link>
+    </div>
+  );
+};
+
+export default UserProgress;
