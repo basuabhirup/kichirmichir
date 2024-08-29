2e8544a8454176d5db80c45ac09709c4a27ec5a0
basuabhirup <basu.abhirup27@gmail.com>
2024-08-29 19:11:25 +0530

    Minor Improvements + Final Touch

--- components/sticky-wrapper.tsx
+++ components/sticky-wrapper.tsx
@@ -1,11 +1,13 @@
-import { PropsWithChildren } from "react";
+import { PropsWithChildren } from "react"
 
 const StickyWrapper: React.FC<PropsWithChildren> = ({ children }) => {
   return (
-    <div className="hidden lg:block w-[360px] self-end z-1 bottom-6">
-      <div className="min-h-[calc(100vh-48px)] sticky top-6 flex flex-col gap-y-4">{children}</div>
+    <div className="hidden lg:block w-[360px] self-start z-1 bottom-6">
+      <div className="min-h-[calc(100vh-48px)] sticky top-6 flex flex-col gap-y-4">
+        {children}
+      </div>
     </div>
-  );
-};
+  )
+}
 
-export default StickyWrapper;
+export default StickyWrapper
